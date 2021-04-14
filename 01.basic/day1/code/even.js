2719de7c65c52e27d0de3fb7eb0116adbceac2d0
mars <zheng.xiangfei@rococo.co.jp>
2021-04-14 17:30:51 +0800

    day2

--- 01.basic/day1/code/even.js
+++ 01.basic/day1/code/even.js
@@ -1,8 +1,16 @@
-const numList = [10,3,7,12,25,87,30,28];
+const numList = [0,3,,12,25,87,30,28];
 for (let i = 0; i < numList.length; i++) {
-    if(numList[i]%2 === 0) {
-        console.log(numList[i]);
+
+    if(numList[i]) {
+        if(numList[i]%2 === 0) {
+            console.log(numList[i]);
+        } else {
+            console.log(numList[i] + "is not even!");
+        }
     } else {
-        console.log(numList[i] + "is not even!");
+        console.log("data : "+i+" 番目 is undefined!");
     }
+
 }
+
+undefined === false
--- 01.basic/day2/code/grammar.js
+++ 01.basic/day2/code/grammar.js
@@ -1,51 +1,107 @@
-let テスト="aaa";
-console.log(テスト);
 
-let test = 0;
+// let テスト="aaa";
+// console.log(テスト);
 
-if(!test) {
-    console.log("aa");
+// let test = 0;
+// let _test = 0;
+// let $test = 0;
+// let test11 = 0
+
+// test12=12;
+// const test13 = "hello"; 
+// console.log(test13);
+// console.log(test12);
+
+// let myArray = [];
+// console.log(myArray[0]); 
+
+// if(!test) {
+//     console.log("aa");
+// }
+
+// console.log(parseInt("0100",2));
+// console.log(parseInt("0100",10));
+// console.log(parseInt("0100",8));
+// console.log(parseFloat("10"));
+// console.log(parseFloat("10.33"));
+// console.log(parseFloat("34 45 66"));
+// console.log(parseFloat(" 60 "));
+// console.log(parseFloat("40 years"));
+// console.log(parseFloat("He was 40"));
+// console.log(parseInt(10n,10));
+
+
+// var unusualPropertyNames = {
+//     '': '空文字列',
+//     '!': 'バン！'
+//   }
+// //   console.log(unusualPropertyNames.'');   // SyntaxError: Unexpected string が発生
+//   console.log(unusualPropertyNames['']);  // 空文字列
+// //   console.log(unusualPropertyNames.!);    // SyntaxError: Unexpected token ! が発生
+//   console.log(unusualPropertyNames['!']); // バン！
+
+//   console.log(2**3);
+//   console.log(2<<3);
+
+//   // 配列
+// var trees = ['redwood', 'bay', 'cedar', 'oak', 'maple'];
+// 0 in trees;        // true を返す
+// 3 in trees;        // true を返す
+// 6 in trees;        // false を返す
+// 'bay' in trees;    // false を返す（インデックスの指す値ではなく、
+//                    // インデックスの数字を指定しなければならない）
+// 'length' in trees; // true を返す（length は Array のプロパティ）
+
+// // 定義済みオブジェクト
+// 'PI' in Math;          // true を返す
+// var myString = new String('coral');
+// 'length' in myString;  // true を返す
+
+// // ユーザー定義オブジェクト
+// var mycar = { make: 'Honda', model: 'Accord', year: 1998 };
+// 'make' in mycar;  // returns true
+// 'model' in mycar; // returns true
+
+// console.log(Number.MAX_SAFE_INTEGER);
+let test = BigInt("9007199254740991");
+console.log(test+1n);
+
+console.log(10n === 10); // → false
+console.log(10n == 10); // → true
+
+//  console.log(+test);
+
+console.log(+"111");
+console.log(parseFloat("111.111"))
+
+let t = "aaaaaaaaaa";
+console.log(t.length);
+console.log(`ssssss${t}ssssss`);
+console.log("ssssss"+t+"ssssss");
+
+let name = { firstName: 'zheng', secondName: 'xiangfei' };
+
+console.log(name.firstName);
+console.log(name.secondName);
+console.log(name["firstName"]);
+name.firstName="bbb";
+console.log(name.firstName);
+
+
+function plus(a,b) {
+    return a + b;
 }
 
-console.log(parseInt("0100",2));
-console.log(parseInt("0100",10));
-console.log(parseInt("0100",8));
-console.log(parseFloat("10"));
-console.log(parseFloat("10.33"));
-console.log(parseFloat("34 45 66"));
-console.log(parseFloat(" 60 "));
-console.log(parseFloat("40 years"));
-console.log(parseFloat("He was 40"));
-console.log(parseInt(10n,10));
+let result = plus(3,2);
+console.log(result);
 
 
-var unusualPropertyNames = {
-    '': '空文字列',
-    '!': 'バン！'
-  }
-//   console.log(unusualPropertyNames.'');   // SyntaxError: Unexpected string が発生
-  console.log(unusualPropertyNames['']);  // 空文字列
-//   console.log(unusualPropertyNames.!);    // SyntaxError: Unexpected token ! が発生
-  console.log(unusualPropertyNames['!']); // バン！
+let f = {};
+let search = "apple";
 
-  console.log(2**3);
-  console.log(2<<3);
+let searchResults = searchPrice(seach);
+console.log(searchResults);
 
-  // 配列
-var trees = ['redwood', 'bay', 'cedar', 'oak', 'maple'];
-0 in trees;        // true を返す
-3 in trees;        // true を返す
-6 in trees;        // false を返す
-'bay' in trees;    // false を返す（インデックスの指す値ではなく、
-                   // インデックスの数字を指定しなければならない）
-'length' in trees; // true を返す（length は Array のプロパティ）
-
-// 定義済みオブジェクト
-'PI' in Math;          // true を返す
-var myString = new String('coral');
-'length' in myString;  // true を返す
-
-// ユーザー定義オブジェクト
-var mycar = { make: 'Honda', model: 'Accord', year: 1998 };
-'make' in mycar;  // returns true
-'model' in mycar; // returns true
+function searchPrice(seach) {
+1111
+}
